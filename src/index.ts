165b7f88d0f486553b2b757d0d1a491922816d21
Aom Siriwat <siriwat576@gmail.com>
2025-07-20 01:20:51 +0700

    chore: Export provider method types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 export * from "./providers";
 export { Explorer, AbstractBlockExplorer } from "./core";
 export { IBlockExplorer } from "./types";
+export type {
+    GetTransactionsOptions,
+    GetTransactionsResponse,
+    GetTokenInfoOptions,
+    GetHoldingTokenOptions,
+    GetHoldingTokenResponse,
+    GetTokenInfoResponse,
+    GetBalanceOptions,
+    GetUTXOsOptions,
+    GetUTXOsResponse,
+    BroadcastOptions,
+} from "./types";
